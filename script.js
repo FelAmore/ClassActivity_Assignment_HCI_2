518f65c624acc1ff1526c41d9f824efd57281558
FelAmore <114073117+FelAmore@users.noreply.github.com>
2022-12-20 22:14:33 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-// Add your answer here
+$('.text-contents').fadeOut()
+
+$('.lesson').hover(
+	function() {
+		$(this).find('.text-contents').fadeIn();
+	},
+	function() {
+		$(this).find('.text-contents').fadeOut();
+	}
+);
